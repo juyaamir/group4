37ec9266f2154df726db7d718c999c0b3e06937a
gulmina <gulmina44@gmail.com>
2024-07-18 13:22:34 +0200

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,25 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
+import usersRoutes from "./routes/UsersAccountRoutes";
+
 const app = express();
 dotenv.config();
 app.use(cors({ origin: "*" }));
 const port = process.env.PORT || 8000;
 
+// database connection
+import connectDB from "./db/db.js";
+connectDB();
+
 app.get("/", (req, res) => {
   res.send(`Hello from Express!`);
+});
+
+app.use("/api/v1/usersaccount", usersRoutes);
+
+app.get("/*", (req, res) => {
+  res.send("invalid endpoint!");
 });
 
 app.listen(port, () => {
---
+++ backend/routes/UsersAccountRoutes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import {
+  getAllUsers,
+  getSingleUser,
+  createSingleUser,
+  updateSingleUser,
+  deleteSingleUser,
+} from "../controllers/UsersAccountController";
+
+const router = Router();
+
+router.get("/", getAllUsers);
+
+router.post("/", createSingleUser);
+
+router.get("/:id", getSingleUser);
+
+router.put("/:id", updateSingleUser);
+router.delete("/:id", deleteSingleUser);
+
+export default router;

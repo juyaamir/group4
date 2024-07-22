c826b42473666096faf482e167ed0d6cf872e036
gulmina <gulmina44@gmail.com>
2024-07-22 13:29:48 +0200

    daiyUI installed as thailwind plugins

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
+import HeroImage from "../components/HeroImage";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <HeroImage />
       <Main />
       <Footer />
     </div>

f4062fc4eebc1c388ce19b758c60daa7c25ce415
gulmina <gulmina44@gmail.com>
2024-07-22 23:58:35 +0200

    one time of db importing line is removed from index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,6 @@ import cors from "cors";
 import connectDB from "./db/db.js";
 import weatherRouter from "./routes/API/weatherRoutes.js";
 
-import connectDB from "./db/db.js";
 import locationRouter from "./routes/API/locationRoutes.js";
 
 import userRouter from "./routes/UsersAccountRoutes.js";
@@ -15,7 +14,7 @@ import orderRouter from "./routes/OrderRouter.js";
 import productRouter from "./routes/ProductRouter.js";
 
 // importing routes
-import userRouter from "./routes/UserRoute/index.js";
+import userRouter2 from "./routes/UserRoute/index.js";
 import loggingRoutes from "./routes/LoginRoute/index.js";
 
 // create application from express
@@ -53,7 +52,6 @@ app.use((req, res, next) => {
   next();
  */
 
-
 //location API route
 app.use("/api/v1/location", locationRouter);
 
@@ -71,7 +69,7 @@ app.get("/*", (req, res) => {
 
 // End points
 app.use("/api/v1/auth", loggingRoutes);
-app.use("/api/v1/usersaccounts", userRouter);
+app.use("/api/v1/usersaccounts", userRouter2);
 
 // app listening port
 const port = process.env.PORT || 8000;

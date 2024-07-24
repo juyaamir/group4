5c44a59613ecd03445d20a7a04350898a7cd2739
gulmina <gulmina44@gmail.com>
2024-07-24 14:19:51 +0200

    items in the Product Page added

--- frontend/src/components/HeroImage.jsx
+++ frontend/src/components/HeroImage.jsx
@@ -12,16 +12,24 @@ const HeroImage = () => {
       >
         <div className="hero-content flex-col lg:flex-col opacity:200">
           <div>
-            <h1 className="text-5xl font-bold my-3">Pack Your Packing-List</h1>
-            <h3 className="text-5xl font-bold my-3">With JourneyPack</h3>
-            <p className=" text-xl my-3 ">
+            <h1 className="text-3xl font-black text-black my-3">Pack Your</h1>
+            <h3 className="text-5xl font-bold text-black my-3">
+              Packing-List With
+            </h3>
+            <h3 className="text-7xl font-medium text-black my-3">
+              JourneyPack
+            </h3>
+            <p className="font-medium text-black text-xl my-3 ">
               If you're confused about what to pack for your trip our app
               <p>
                 <strong className="font-white">&nbsp;JourneyPack&nbsp;</strong>
                 is here to help
               </p>
             </p>
-            <p>Don't have an account? No worries! Sign up now</p>
+            <p className="font-normal text-black text-xl my-3 ">
+              Don't have an account? No worries!<strong>&nbsp;Sign up</strong>
+              &nbsp; now
+            </p>
             <Link to={`/signup`}>
               <button className="btn btn-primary my-3">Sign up</button>
             </Link>

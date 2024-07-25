3974fc327cb75a406de0f7e8284471a0cb8eaace
gulmina <gulmina44@gmail.com>
2024-07-25 05:57:36 +0200

    in products-page , products are shown based on its category

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import ProductCard from "../components/ProductCard";
 
 const Product = () => {
   return (
@@ -17,66 +17,7 @@ const Product = () => {
             className="tab-content bg-base-100 border-base-300 rounded-box p-6"
           >
             <div className="flex gap-4">
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 499€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/product-square-680/439865/image.jpg"
-                    alt="bags"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 99€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/product-square-680/76873/image.jpg"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 199€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/sqaure-1360/468031/image.jpg"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 799€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
+              <ProductCard category={"Household item"} />
             </div>
           </div>
 
@@ -93,66 +34,7 @@ const Product = () => {
             className="tab-content bg-base-100 border-base-300 rounded-box p-6"
           >
             <div className="flex gap-4">
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://assets.bounceexchange.com/assets/uploads/clients/5384/creatives/810b0c4895fb9e1e937f460bb81f1438.jpg"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Wrist Watch!</h2>
-                  <p>Price : 499€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/bd57993750aa132a0acaadce4ded3ea2.jpg?imageView2/2/w/800/q/70"
-                    alt="bags"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Sun Glasses !</h2>
-                  <p>Price : 99€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/product-square-680/76873/image.jpg"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Luggage Bag</h2>
-                  <p>Price : 199€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/sqaure-1360/468031/image.jpg"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Bag!</h2>
-                  <p>Price : 799€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
+              <ProductCard category={"Electronics"} />
             </div>
           </div>
 
@@ -168,68 +50,10 @@ const Product = () => {
             className="tab-content bg-base-100 border-base-300 rounded-box p-6"
           >
             <div className="flex gap-4">
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 499€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/product-square-680/439865/image.jpg"
-                    alt="bags"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 99€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/product-square-680/76873/image.jpg"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 199€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/sqaure-1360/468031/image.jpg"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 799€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
+              <ProductCard category={"Sport"} />
             </div>
           </div>
+
           <input
             type="radio"
             name="my_tabs_2"
@@ -242,66 +66,7 @@ const Product = () => {
             className="tab-content bg-base-100 border-base-300 rounded-box p-6"
           >
             <div className="flex gap-4">
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 499€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/product-square-680/439865/image.jpg"
-                    alt="bags"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 99€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/product-square-680/76873/image.jpg"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 199€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-              <div className="card card-compact bg-base-100 w-60 shadow-xl">
-                <figure>
-                  <img
-                    src="https://media.travelbags-cdn.nl/sqaure-1360/468031/image.jpg"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>Price : 799€</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
+              <ProductCard category={"Accessories"} />
             </div>
           </div>
         </div>

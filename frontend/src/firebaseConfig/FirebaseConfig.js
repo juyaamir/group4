89a0bdba082cb7a7f36b9a7e21d0ab6f229fe369
gulmina <gulmina44@gmail.com>
2024-07-28 17:18:30 +0200

    changes added

---
+++ frontend/src/firebaseConfig/FirebaseConfig.js
@@ -0,0 +1,7 @@
+import {initializeapp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
+import {getStorage} from 'firebase/storage';
+
+const firebaseConfig = {
+    api

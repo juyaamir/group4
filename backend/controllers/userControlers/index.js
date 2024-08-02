bafaab73b0596795116c8326470aac1c9645ed47
Muhammad-N-Ali <m.naeemali572@gmail.com>
2024-08-02 18:35:19 +0200

    updating about page

--- backend/controllers/userControlers/index.js
+++ backend/controllers/userControlers/index.js
@@ -69,21 +69,54 @@ export const getOne = async (req, res) => {
 
 export const updateOne = async (req, res) => {
   try {
-    const user = await UsersAccount.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+    const {id}=req.params
+    const user = await UsersAccount.findById(id)
+    if(!user){
+      return res.status(404).json({message:"User not found"})
     }
-    res.json(user);
+    
+    if (user) {
+      console.log("user",req.body)
+      user.firstname = req.body.firstname || user.firstname
+      user.lastname = req.body.lastname || user.lastname
+      user.email = req.body.email || user.email
+      if (req.body.password) {
+        // const salt = await bcrypt.genSalt(10);
+        // const pass = await bcrypt.hash(req.body.password, salt);
+        user.password = req.body.password
+      }
+  
+      const updatedUser = await user.save()
+      console.log("updatedUser",updatedUser)
+  
+      res.json(updatedUser)
+      // res.json({
+      //   _id: updatedUser._id,
+      //   name: updatedUser.name,
+      //   email: updatedUser.email,
+      //   isAdmin: updatedUser.isAdmin,
+      //   token: generateToken(updatedUser._id),
+      // })
+    }
+  // }
+
+    // const newUser = req.body
+    // console.log(newUser, pass)
+    // const user = await UsersAccount.findByIdAndUpdate(req.params.id, req.body, {
+      // new: true,
+      // runValidators: true,
+    // });
+    // if (!user) {
+    //   return res.status(404).json({ message: "User not found" });
+    // }
+    // res.json(user);
   } catch (error) {
     console.error(error);
     res
       .status(500)
       .json({ message: "Internal Server Error", error: error.message });
   }
-};
+}
 
 //===========================
 // Delete One

5635984f0e6bbd959cb71a225cc98b99c99bf06a
Muhammad-N-Ali <m.naeemali572@gmail.com>
2024-08-07 00:47:41 +0200

    footer updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -14,43 +14,43 @@ const Footer = () => {
 
   return (
     <>
-      <footer className="footer bg-neutral text-neutral-content p-1 justify-around ">
-        <aside className="flex items-center ">
+      <footer className="footer bg-neutral text-neutral-content justify-around ">
+        <aside className="flex items-center py-6 ">
           <img src={logo} alt="logo" className="h-12 w-12 rounded-full mt-3" />
-          <h1 className="text-xl font-bold mt-3">JourneyPack</h1>
+          <h1 className="text-3xl text-yellow-600 font-DM1 mt-3">JourneyPack</h1>
         </aside>
 
         {/* Subscribe section */}
-        <div className="p-1 rounded-lg mx-auto">
-          <h2 className="font-OpenSans text-xl font-semibold italic tracking-widest text-white text-center py-3">
-            Subscribe to our Newsletter
-          </h2>
+        <div className="flex flex-col py-6">
+            <p className="font-bold">Newsletter Subscription</p>
+          
+          
           <form onSubmit={handleSubmit} className="flex items-center">
             <input
               type="email"
-              placeholder="Enter your email"
+              placeholder="Subscribe to our newsletter"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="input input-bordered text-black w-full max-w-xs mr-2"
+              className="h-8 text-black w-50 pl-2 pr-8 text-center border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent"
               required
             />
             <button
               type="submit"
-              className="h-10 w-50 rounded-full bg-blue-400 px-4 py-2 text-center font-OpenSans text-xl text-white hover:bg-blue-700"
+              className="h-8 w-50 bg-yellow-600 px-3 text-center text-white hover:bg-yellow-700"
             >
               Subscribe
             </button>
           </form>
         </div>
 
-        <nav className="py-1">
-          <h6 className="footer-title">Follow Us</h6>
-          <div className="grid grid-flow-col gap-4 text-blue-300 text-2xl">
+        <nav className=" flex flex-col py-6">
+        <p className="font-bold">Get In Touch</p>
+          <div className="grid grid-flow-col gap-2 text-blue-300 text-2xl">
             <a
               href="https://www.facebook.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="link link-hover hover:text-blue-500"
+              className="link link-hover text-blue-500 hover:text-blue-700"
             >
               <i className="fa-brands fa-facebook"></i>
             </a>
@@ -58,7 +58,7 @@ const Footer = () => {
               href="https://www.instagram.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="link link-hover hover:text-blue-500"
+              className="link link-hover text-red-400 hover:text-orange-600"
             >
               <i className="fa-brands fa-instagram"></i>
             </a>
@@ -66,7 +66,7 @@ const Footer = () => {
               href="https://www.twitter.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="link link-hover hover:text-blue-500"
+              className="link link-hover text-blue-300 hover:text-blue-500"
             >
               <i className="fa-brands fa-square-twitter"></i>
             </a>
@@ -74,9 +74,17 @@ const Footer = () => {
               href="https://www.linkedin.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="link link-hover hover:text-blue-500"
+              className="link link-hover text-blue-400 hover:text-blue-600"
             >
               <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a
+              href="https://www.youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link link-hover text-blue-400 hover:text-blue-600"
+            >
+              <i className="fa-brands fa-youtube text-red-500 hover:text-red-700 text-2xl"></i>
             </a>
           </div>
         </nav>
@@ -84,7 +92,7 @@ const Footer = () => {
 
       <div className="bg-gray-900 text-white text-[14px] py-4 text-center">
         <p className="">
-          &copy; {new Date().getFullYear()} JourneyPack. All rights reserved.
+          &copy; {new Date().getFullYear()} JourneyPack All rights reserved
         </p>
       </div>
     </>

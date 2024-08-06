05c7543ca9628222a7e3bc0afed05d21059babc5
Juya <juyaamirm@gmail.com>
2024-08-06 12:34:21 +0200

    deploy

--- frontend/src/components/Stores.jsx
+++ frontend/src/components/Stores.jsx
@@ -162,7 +162,7 @@ const Stores = () => {
       </div>
 
       <div className='mb-6'>
-        <h1 className='text-center text-xl my-4'>ADVERTISEMENT</h1>
+        <h1 className='text-center text-xl mt-6 mb-8'>ADVERTISEMENT</h1>
         <div className=' mx-10 justify-between '>
           <HotelCard />
         </div>

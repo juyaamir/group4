aa957ee01d0eef2dc1a6830a25f04ffbaaf80f8a
gulmina <gulmina44@gmail.com>
2024-08-07 18:44:27 +0200

    profile img deployed

--- backend/routes/chatRouter.js
+++ backend/routes/chatRouter.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { Router } from "express";
 import { createChat } from "../controllers/Chats.js";
-=======
-import { Router } from 'express';
-import { createChat } from '../controllers/Chats.js'
->>>>>>> 5804a2b1958d3ad1e4fd49456bd9b5fdfc664479
 
 const chatRouter = Router();
 
